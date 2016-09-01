119e673bb6640c02dfd6398cf4a4b0f52bc298eb
Alexey Shmalko <rasen.dubi@gmail.com>
2016-09-01 13:44:03 +0300

    KAA-1377 Refactor RangeExpressionParser

--- server/common/transport-shared/src/main/java/org/kaaproject/kaa/server/transport/RangeExpressionParser.java
+++ server/common/transport-shared/src/main/java/org/kaaproject/kaa/server/transport/RangeExpressionParser.java
@@ -17,115 +17,58 @@
 package org.kaaproject.kaa.server.transport;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * Converts a range expression ("1-3, 2-7, 15") into a sequence of numbers from that range
- *
- * @author Oleg Klishch
- * 
  */
 public final class RangeExpressionParser {
 
-    private static final String RANGE_DELIMITER = ",";
-    private static final String CORNERS_DELIMITER = "-";
-    private static final int RANGE_START = 0;
-    private static final int RANGE_END = 1;
+    private static final String RANGE_DELIMITER_REGEX = ",";
+    private static final String BOUNDS_DELIMITER_REGEX = "-";
 
     public List<Integer> getNumbersFromRanges(String expression) {
         if (expression == null) {
             throw new IllegalArgumentException("Expression can not be null");
         }
 
-        List<Integer> numbersFromRanges = new ArrayList<>();
-        List<Range> ranges = parseRanges(expression);
-        for (Range range : mergeRanges(ranges)) {
-            for (int i = range.getFrom(); i <= range.getTo(); i++) {
-                numbersFromRanges.add(i);
+        // Using a TreeSet to retrieve a sorted list of numbers in result
+        TreeSet result = new TreeSet<Integer>();
+        for (Range range : parseRanges(expression)) {
+            for (int i = range.from; i <= range.to; i++) {
+                result.add(i);
             }
         }
-        return numbersFromRanges;
+        return new ArrayList<>(result);
     }
 
     private List<Range> parseRanges(String expression) {
-        String[] rangeStrings = expression.split(RANGE_DELIMITER);
         List<Range> ranges = new ArrayList<>();
-        for (String rangeString : rangeStrings) {
-            ranges.add(parseRange(rangeString.trim()));
+        for (String range : expression.split(RANGE_DELIMITER_REGEX)) {
+            ranges.add(parseRange(range));
         }
         return ranges;
     }
 
-    private Range parseRange(String rangeString) {
-        if (!rangeString.contains(CORNERS_DELIMITER)) {
-            int from = Integer.parseInt(rangeString);
-            return new Range(from, from);
+    private Range parseRange(String range) {
+        String[] bounds = range.split(BOUNDS_DELIMITER_REGEX);
+        if (bounds.length != 1 && bounds.length != 2) {
+            throw new IllegalArgumentException("Wrong number of bounds for range");
         }
 
-        String[] cornerStrings = rangeString.split(CORNERS_DELIMITER);
-        int from = Integer.parseInt(cornerStrings[RANGE_START]);
-        int to = Integer.parseInt(cornerStrings[RANGE_END]);
+        int from = Integer.parseInt(bounds[0].trim());
+        int to = (bounds.length == 2) ? Integer.parseInt(bounds[1].trim()) : from;
         return new Range(from, to);
     }
 
-    private List<Range> mergeRanges(List<Range> ranges) {
-        if (ranges.isEmpty()) {
-            return ranges;
-        }
-
-        List<Range> mergedRanges = new ArrayList<>();
-        Collections.sort(ranges, new RangeComparator());
-        Range currentRange = ranges.get(0);
-        for (int i = 1; i < ranges.size(); i++) {
-            if (currentRange.getTo() >= ranges.get(i).getFrom()) {
-                currentRange.setTo(Math.max(currentRange.getTo(), ranges.get(i).getTo()));
-            } else {
-                mergedRanges.add(currentRange);
-                currentRange = ranges.get(i);
-            }
-        }
-        mergedRanges.add(currentRange);
-        return mergedRanges;
-    }
-
     private static class Range {
-
-        private int from;
-        private int to;
+        public int from;
+        public int to;
 
         private Range(int from, int to) {
             this.from = from;
             this.to = to;
-        }
-        
-        public int getFrom() {
-            return from;
-        }
-
-        public void setFrom(int from) {
-            this.from = from;
-        }
-
-        public int getTo() {
-            return to;
-        }
-
-        public void setTo(int to) {
-            this.to = to;
-        }
-    }
-
-    private static class RangeComparator implements Comparator<Range> {
-
-        @Override
-        public int compare(Range first, Range second) {
-            int cmp = Integer.compare(first.getFrom(), second.getFrom());
-            if (cmp != 0) {
-                return cmp;
-            }
-            return Integer.compare(second.getTo(), second.getTo());
         }
     }
 }
